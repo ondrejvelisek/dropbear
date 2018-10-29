b2a2432360ec60e1bbd8b04505b0ae23c8c9a599
ondrejvelisek <ondrejvelisek@gmail.com>
2018-10-29 16:21:04 +0100

    Fix reading EOL in authorization mapping Logging update in authorization

--- oauth2-authorization.c
+++ oauth2-authorization.c
@@ -48,10 +48,11 @@ int read_user_record(char* local_username, char* remote_subjects, FILE* file){
 }
 
 short is_present_in_etc(char* local_username, char* remote_subject) {
-    dropbear_log(LOG_ERR, "is_present_in_etc enter");
+    dropbear_log(LOG_INFO, "is_present_in_etc enter");
 
     FILE* authorization_file = open_authorization_file();
     if (authorization_file == NULL) {
+        dropbear_log(LOG_INFO, "Authorization mapping file not found in /etc/");
         return 0;
     }
 
@@ -59,35 +60,38 @@ short is_present_in_etc(char* local_username, char* remote_subject) {
     char remote_subjects_tmp[10000];
     while (read_user_record(local_username_tmp, remote_subjects_tmp, authorization_file) >= 0) {
         if (strcmp(local_username, local_username_tmp) == 0) {
-            dropbear_log(LOG_ERR, "Authorization record for user %s found, %s", local_username, remote_subjects_tmp);
+            dropbear_log(LOG_INFO, "Authorization record for user %s found, %s", local_username, remote_subjects_tmp);
             if (str_set_contains(remote_subjects_tmp, remote_subject, ':')) {
-                dropbear_log(LOG_ERR, "Record contains remote subject %s. Authorizing.", remote_subject);
+                dropbear_log(LOG_INFO, "Record contains remote subject %s. Authorizing.", remote_subject);
                 fclose(authorization_file);
                 return 1;
             }
-            dropbear_log(LOG_ERR, "Record for user %s does not contain remote subject %s", local_username, remote_subject);
+            dropbear_log(LOG_INFO, "Record for user %s does not contain remote subject %s", local_username, remote_subject);
         }
     }
     fclose(authorization_file);
 
-    dropbear_log(LOG_ERR, "Authorization record for user %s not found in /etc/", local_username);
+    dropbear_log(LOG_INFO, "Authorization record for user %s not found in /etc/", local_username);
 }
 
 short is_present_in_home(char* remote_subject) {
-    dropbear_log(LOG_ERR, "is_present_in_home enter");
+    dropbear_log(LOG_INFO, "is_present_in_home enter");
 
     FILE* authorization_home_file = open_authorization_home_file();
     if (authorization_home_file == NULL) {
+        dropbear_log(LOG_INFO, "Authorization mapping file not found in home folder");
         return 0;
     }
 
     char* line = NULL;
-    size_t len;
+    size_t len = 0;
     if (getline(&line, &len, authorization_home_file) < 0) {
+        dropbear_log(LOG_INFO, "Unable to read line");
         return 0;
     }
+    line[strcspn(line, "\n")] = '\0';
     if (str_set_contains(line, remote_subject, ':')) {
-        dropbear_log(LOG_ERR, "Record contains remote subject %s found in home. Authorizing.", remote_subject);
+        dropbear_log(LOG_INFO, "Authorization mapping contains remote subject %s", remote_subject);
         free(line);
         fclose(authorization_home_file);
         return 1;
@@ -96,18 +100,22 @@ short is_present_in_home(char* remote_subject) {
         free(line);
     }
 
-    dropbear_log(LOG_ERR, "Authorization record not found in home");
+    dropbear_log(LOG_INFO, "Authorization mapping not found in home");
     return 0;
 }
 
 //////////////////// API ///////////////
 
 short is_authorized(char* local_username, char* remote_subject) {
-    dropbear_log(LOG_ERR, "Authorizing remote subject %s for access to local user %s", remote_subject, local_username);
+    dropbear_log(LOG_INFO, "Authorizing remote subject %s for access to local user %s", remote_subject, local_username);
 
     char authorized = 0;
     authorized = is_present_in_etc(local_username, remote_subject) || is_present_in_home(remote_subject);
 
-    dropbear_log(LOG_ERR, "Authorization %d? record for user %s ", authorized, local_username);
+    if (authorized) {
+        dropbear_log(LOG_INFO, "Remote subject %s authorized to access local user %s", remote_subject, local_username);
+    } else {
+        dropbear_log(LOG_INFO, "Remote subject %s not authorized to access local user %s", remote_subject, local_username);
+    }
     return authorized;
 }
